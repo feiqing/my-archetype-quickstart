0a170cbf9d238e1cb9adf8248cdaf16acd06deb1
feiqing <zhujifang666@163.com>
2016-10-30 08:38:12 +0800

    init

---
+++ src/main/java/com/fq/archetypes/App.java
@@ -0,0 +1,12 @@
+package com.fq.archetypes;
+
+/**
+ * @author jifang
+ * @since 16/10/9 下午6:05.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
